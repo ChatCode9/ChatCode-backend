d2c8388bb9bf9e0b6c7b97386c5d343c89980d0b
minje0204 <minje@ajou.ac.kr>
2024-04-23 15:20:04 +0900

    chore: 복구

---
+++ src/test/java/com/chatcode/ChatcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
