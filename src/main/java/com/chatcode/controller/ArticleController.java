1ab69b82a1d029ac49da0dac3f49e4134c9e091b
eunzz6 <djdj5267@naver.com>
2024-05-07 17:41:44 +0900

    CRUD 기능 구현: PR 피드백 반영2

--- src/main/java/com/chatcode/controller/ArticleController.java
+++ src/main/java/com/chatcode/controller/ArticleController.java
@@ -2,12 +2,12 @@ package com.chatcode.controller;
 
 import com.chatcode.dto.ArticleRequestDTO;
 import com.chatcode.dto.ArticleRequestDTO.ArticleUpdateRequestDTO;
-import com.chatcode.dto.ArticleResponseDTO.ArticleCreateResponseDTO;
 import com.chatcode.dto.BaseResponseDto;
 import com.chatcode.service.ArticleService;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,36 +21,36 @@ public class ArticleController {
     }
 
 
-    @PostMapping("/create")
-    public BaseResponseDto<ArticleCreateResponseDTO> createArticle(@Valid @RequestBody ArticleRequestDTO.ArticleCreateRequestDTO params) {
+    @PostMapping("")
+    public ResponseEntity<BaseResponseDto<Void>> createArticle(@Valid @RequestBody ArticleRequestDTO.ArticleCreateRequestDTO params) {
         articleService.articleCreate(params);
-        return new BaseResponseDto<>(1, null, "성공적으로 게시글이 등록되었습니다.");
+        return ResponseEntity.ok(new BaseResponseDto<>(1, null, "성공적으로 게시글이 등록되었습니다."));
     }
 
-    @PostMapping("/update/{articleId}")
-    public BaseResponseDto<ArticleCreateResponseDTO> updateArticle(@PathVariable Long articleId, @RequestBody ArticleUpdateRequestDTO params) {
+    @PutMapping("/{articleId}")
+    public ResponseEntity<BaseResponseDto<Void>> updateArticle(@PathVariable Long articleId, @RequestBody ArticleUpdateRequestDTO params) {
         articleService.articleUpdate(articleId, params);
-        return new BaseResponseDto<>(1, null, "업데이트 성공");
+        return ResponseEntity.ok(new BaseResponseDto<>(1, null, "업데이트 성공"));
     }
 
-    @GetMapping("/list")
-    public BaseResponseDto<List<String>> getArticleList() {
+    @GetMapping("")
+    public ResponseEntity<BaseResponseDto<List<String>>> getArticleList() {
         List<String> articleList = articleService.readArticleList();
-        return new BaseResponseDto<>(1, articleList, "게시글 목록 조회 성공");
+        return ResponseEntity.ok(new BaseResponseDto<>(1, articleList, "게시글 목록 조회 성공"));
     }
 
     @GetMapping("/{articleId}")
-    public BaseResponseDto<String> readArticleById(@PathVariable Long articleId) {
+    public ResponseEntity<BaseResponseDto<Object>> readArticleById(@PathVariable Long articleId) {
         Optional<String> contentText = articleService.readArticleById(articleId);
         if (contentText.isPresent()) {
-            return new BaseResponseDto<>(1, contentText.get(), "글 내용 조회 성공");
+            return ResponseEntity.ok(new BaseResponseDto<>(1, contentText.get(), "글 내용 조회 성공"));
         }
-        return new BaseResponseDto<>(0, null, "해당 제목에 대한 글 내용이 없습니다.");
+        return ResponseEntity.ok(new BaseResponseDto<>(0, null, "해당 제목에 대한 글 내용이 없습니다."));
     }
 
-    @DeleteMapping("/delete/{articleId}")
-    public BaseResponseDto<ArticleCreateResponseDTO> deleteArticleAndContent(@PathVariable Long articleId) {
+    @DeleteMapping("/{articleId}")
+    public ResponseEntity<BaseResponseDto<Void>> deleteArticleAndContent(@PathVariable Long articleId) {
         articleService.deleteArticle(articleId);
-        return new BaseResponseDto<>(1, null, "포스트 삭제 성공");
+        return ResponseEntity.ok(new BaseResponseDto<>(1, null, "포스트 삭제 성공"));
     }
 }
