73f7f7ed62d8813a96e80e1ff7227d08e8bf0bef
Kim JunHyun <jjag1015@naver.com>
2024-05-08 23:06:47 +0900

    feat(category): New custom exception 추가.

--- src/main/java/com/chatcode/exception/GlobalExceptionHandler.java
+++ src/main/java/com/chatcode/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.chatcode.exception;
 
 import com.chatcode.dto.BaseResponseDto;
+import com.chatcode.exception.common.ContentNotFoundException;
 import com.chatcode.exception.common.ResourceNotFoundException;
 import com.chatcode.exception.reaction.AlreadyReactException;
 import org.springframework.http.HttpStatus;
@@ -11,14 +12,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(AlreadyReactException.class)
-  public ResponseEntity<BaseResponseDto<Void>> handleAlreadyReactException(AlreadyReactException ex) {
-    //Todo 에러 코드 정하기
-    return ResponseEntity.badRequest().body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null ,ex.getMessage()));
-  }
+    @ExceptionHandler(AlreadyReactException.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleAlreadyReactException(AlreadyReactException ex) {
+        //Todo 에러 코드 정하기
+        return ResponseEntity.badRequest()
+                .body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null, ex.getMessage()));
+    }
 
-  @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<BaseResponseDto<Void>> handleResourceNotFoundException(ResourceNotFoundException ex) {
-    return ResponseEntity.badRequest().body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null ,ex.getMessage()));
-  }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.badRequest()
+                .body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ContentNotFoundException.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleContentNotFoundException(ContentNotFoundException ex) {
+        return ResponseEntity.badRequest()
+                .body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null, ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<BaseResponseDto<Void>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest()
+                .body(new BaseResponseDto<>(HttpStatus.BAD_REQUEST.value(), null, ex.getMessage()));
+    }
 }
---
+++ src/main/java/com/chatcode/exception/category/IllegalCategoryOrderExcetion.java
@@ -0,0 +1,7 @@
+package com.chatcode.exception.category;
+
+public class IllegalCategoryOrderExcetion extends IllegalArgumentException {
+    public IllegalCategoryOrderExcetion(String message) {
+        super(message);
+    }
+}
