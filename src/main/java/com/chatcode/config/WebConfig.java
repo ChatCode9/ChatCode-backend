e15964206c84065ed764fdbb6e18379b42a64e5f
minje0204 <minje@ajou.ac.kr>
2024-05-21 16:09:33 +0900

    chore: converter bean으로 변경

--- src/main/java/com/chatcode/config/WebConfig.java
+++ src/main/java/com/chatcode/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.chatcode.config;
 
 import com.chatcode.domain.file.Base64ToMultipartFileConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final Base64ToMultipartFileConverter base64ToMultipartFileConverter;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new Base64ToMultipartFileConverter());
+        registry.addConverter(base64ToMultipartFileConverter);
     }
 }
