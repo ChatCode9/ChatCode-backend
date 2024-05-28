2fe3a4e614bffae53fefaa7fd3fad565113b9bb6
Kim JunHyun <jjag1015@naver.com>
2024-05-28 16:40:29 +0900

    feat(OAuth2): Cookie에서 redirect url 확인 및 쿼리 파라미터 등록

--- src/main/java/com/chatcode/config/auth/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/chatcode/config/auth/oauth/OAuth2SuccessHandler.java
@@ -3,50 +3,51 @@ package com.chatcode.config.auth.oauth;
 import com.chatcode.config.auth.LoginUser;
 import com.chatcode.config.auth.jwt.JwtProvider;
 import com.chatcode.config.auth.jwt.JwtSettings;
-import com.chatcode.config.auth.oauth.dto.OAuth2SuccessResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
-    private static final String CONTENT_TYPE = "application/json";
-    private static final String CHARSET = "UTF-8";
     private static final String COOKIE_PATH = "/";
+    private static final String REDIRECT_COOKIE_KEY = "redirectUrl";
 
     private final JwtProvider jwtProvider;
     private final JwtSettings jwtSettings;
-    private final ObjectMapper om;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
-        String accessToken = jwtProvider.createAccessToken(loginUser);
+        LoginUser user = (LoginUser) authentication.getPrincipal();
+        String accessToken = jwtProvider.createAccessToken(user);
+        String redirectUrl = getRedirectUrl(request);
 
         response.addCookie(createCookie(jwtSettings.getAccessKey(), accessToken));
 
-        response.setContentType(CONTENT_TYPE);
-        response.setCharacterEncoding(CHARSET);
-        response.getWriter().write(createResponseBody(loginUser));
+        String responseUrl = UriComponentsBuilder.fromUriString(redirectUrl)
+                .queryParam("success", true)
+                .queryParam("profile", user.getStatus())
+                .build()
+                .toUriString();
+
+        response.sendRedirect(responseUrl);
     }
 
-    private String createResponseBody(LoginUser loginUser) throws JsonProcessingException {
-        Boolean isProfileChanged = loginUser.getStatus() != 0;
-        return om.writeValueAsString(new OAuth2SuccessResponse(
-            isProfileChanged,
-            loginUser.getId(),
-            loginUser.getAvatarId()
-        ));
+    private String getRedirectUrl(HttpServletRequest request) {
+        return Arrays.stream(request.getCookies())
+            .filter(cookie -> REDIRECT_COOKIE_KEY.equals(cookie.getName()))
+            .findFirst()
+            .map(Cookie::getValue)
+            .orElse("/");
     }
 
     private Cookie createCookie(String key, String value) {
--- src/main/java/com/chatcode/config/auth/oauth/dto/OAuth2SuccessResponse.java
+++
@@ -1,8 +0,0 @@
-package com.chatcode.config.auth.oauth.dto;
-
-public record OAuth2SuccessResponse(
-        Boolean isProfileChanged,
-        Long userId,
-        Long avatarId
-) {
-}
