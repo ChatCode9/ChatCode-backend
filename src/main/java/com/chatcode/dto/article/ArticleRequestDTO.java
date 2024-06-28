6362010c15d289fc1a956b53cddf9ba1e902a00f
Kim JunHyun <jjag1015@naver.com>
2024-06-28 16:46:38 +0900

    feat(requestDto): request DTO message 추가

--- src/main/java/com/chatcode/dto/article/ArticleRequestDTO.java
+++ src/main/java/com/chatcode/dto/article/ArticleRequestDTO.java
@@ -12,11 +12,11 @@ public class ArticleRequestDTO {
         //content
         @NotNull
         @NotEmpty
-        @Size(max = 10_000)
+        @Size(max = 10_000, message = "최대 10,000자 까지 입력 가능합니다.")
         private String contentText;
 
         //article
-        @Size(max = 10)
+        @Size(max = 100, message = "최대 100자 까지 입력 가능합니다.")
         @NotNull
         @NotEmpty
         private String title;
@@ -27,12 +27,12 @@ public class ArticleRequestDTO {
         //content
         @NotNull
         @NotEmpty
-        @Size(max = 10_000)
+        @Size(max = 10_000, message = "최대 10,000자 까지 입력 가능합니다.")
         private String contentText;
         private int contentId;
 
         //article
-        @Size(max = 100)
+        @Size(max = 100, message = "최대 100자 까지 입력 가능합니다.")
         @NotNull
         @NotEmpty
         private String title;
