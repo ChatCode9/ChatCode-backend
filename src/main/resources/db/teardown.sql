6b7d3ef56186686202d1ce9e7850edcd8e0e5cfd
Kim JunHyun <jjag1015@naver.com>
2024-06-28 17:00:42 +0900

    chore(teardown): truncate table 추가

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -1,6 +1,9 @@
 SET FOREIGN_KEY_CHECKS = FALSE;
     truncate table category;
     truncate table avatar;
+    truncate table role;
+    truncate table user;
+    truncate table user_role;
     truncate table follow;
     truncate table interest_tag;
 SET FOREIGN_KEY_CHECKS = TRUE;
